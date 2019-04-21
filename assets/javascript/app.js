0914ac5c4d26290cc96eefdcec0f35dac1bc8bec
Jim Tatarakis <jimtatarakis@gmail.com>
2019-04-21 02:47:13 -0700

    first template complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBg9V77ZtVtV56zPJbumSLos-SWIOsI4EQ",
+    authDomain: "train-hw-e4205.firebaseapp.com",
+    databaseURL: "https://train-hw-e4205.firebaseio.com",
+    projectId: "train-hw-e4205",
+    storageBucket: "train-hw-e4205.appspot.com",
+    messagingSenderId: "512287480601"
+  };
+  firebase.initializeApp(config);
